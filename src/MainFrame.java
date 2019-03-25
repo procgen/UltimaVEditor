ab38ae3242482e7e5fc0ee2bf936cad1816e42ad
THEJOSHBOX2000\Josh <joshlorenzen49@gmail.com>
2019-03-25 14:05:16 -0700

    Reorganize main frame. Add more offset data fields.

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
 
-
     MainFrame() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Ultima V Save Editor");
@@ -13,11 +12,34 @@ public class MainFrame extends JFrame {
 
         this.setLayout(new GridLayout(0, 1));
 
-        this.add(makeDataField("STR", 14, 1));
-        this.add(makeDataField("DEXT", 15, 1));
-        this.add(makeDataField("INT", 16, 1));
+        this.add(makeDataField("GOLD", 0x204, 2));
+        this.add(makeDataField("KEYS", 0x206, 1));
+        this.add(makeDataField("GEMS", 0x207, 1));
 
 
+        this.add(makeDataField("STR", 0x0E, 1));
+        this.add(makeDataField("DEXT", 0x0F, 1));
+        this.add(makeDataField("INT", 0x10, 1));
+        this.add(makeDataField("HP", 0x12, 2));
+        this.add(makeDataField("HM", 0x14, 2));
+        this.add(makeDataField("EXP", 0x16, 2));
+
+        this.add(makeButtonPanel());
+
+
+        pack();
+    }
+
+    private JPanel makeDataField(String label, int offset, int length){
+        JPanel p = new JPanel(new FlowLayout());
+        JLabel nameLabel = new JLabel(label + ": ", JLabel.LEADING);
+        DataField nameField = new DataField(offset, length);
+        p.add(nameLabel);
+        p.add(nameField);
+        return p;
+    }
+
+    private JPanel makeButtonPanel(){
         JPanel p = new JPanel(new FlowLayout());
 
         JButton refreshButton = new JButton();
@@ -43,19 +65,6 @@ public class MainFrame extends JFrame {
 
         p.add(refreshButton);
         p.add(applyButton);
-
-        this.add(p);
-
-
-        pack();
-    }
-
-    private static JPanel makeDataField(String label, int offset, int length){
-        JPanel p = new JPanel(new FlowLayout());
-        JLabel nameLabel = new JLabel(label + ": ", JLabel.LEADING);
-        DataField nameField = new DataField(offset, length);
-        p.add(nameLabel);
-        p.add(nameField);
         return p;
     }
 
