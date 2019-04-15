58850a24269760e1c728ad073cb350c8d640165f
procgen <joshlorenzen49@gmail.com>
2019-04-15 16:29:25 -0700

    Add comments all over the place. Change package.

--- src/ultimaeditor/DataField.java
+++ src/ultimaeditor/DataField.java
@@ -1,4 +1,4 @@
-
+package ultimaeditor;
 
 import javax.swing.*;
 
@@ -11,13 +11,25 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.util.Arrays;
 
-public class
-DataField extends JTextField {
 
-    private static Path filePath = Paths.get("G:/Documents/CECS 378/Ultima_5/SAVED.GAM");
+/**
+ * Class that represents a single value in a set of bytes that we want to modify using a gui
+ * saveData is a static byte array that stores the entire SAVED.GAM file to be read from and written to
+ * Each individual DataField can has an offset that tells its location from the beginning of the bytearray and
+ * has a length value that tells how many bytes are represented by the DataField. It also stores the value within the
+ * object itself for displaying in the GUI
+ * The class extends JTextField to facilitate its use in the GUI as an editable textfield.
+ *
+ */
+public class DataField extends JTextField {
+
+//    private static Path filePath = Paths.get("G:/Documents/CECS 378/Ultima_5/SAVED.GAM");
+//    private static Path filePath = Paths.get("/home/josh/Documents/Projects/UltimaV/Ultima_5/Ultima_5/SAVED.GAM");
+    private static Path filePath = Paths.get("./SAVED.GAM");
     private static byte[] saveData;
     public static String[] charNames = new String[16];
 
+    // Static block - When this class is loaded we call 'reload' to read the SAVED.GAM file and store it in saveData
     static {
         reload();
     }
@@ -26,6 +38,7 @@ DataField extends JTextField {
     private int offset;
     private byte[] data;
 
+    // Static method that takes the temporary data from saveData and writes it to disk
     public static void persist(){
         try{
             Files.write(filePath, saveData, StandardOpenOption.TRUNCATE_EXISTING);
@@ -35,9 +48,11 @@ DataField extends JTextField {
         }
     }
 
+    // Reads the SAVED.GAM file and stores it into saveData.
+    // It also converts the bytes of each character name into a string for use in the GUI.
     public static void reload(){
         try{
-            saveData = Files.readAllBytes(filePath);
+            saveData = Files.readAllBytes(filePath); // Read the SAVED.GAM file and store
         }
         catch(IOException e)
         {
@@ -47,7 +62,7 @@ DataField extends JTextField {
 
         for(int i = 0; i < 16; i++)
         {
-            String name = "";
+            String name = ""; //Convert each of the 16 character's names into a string
             for(int j = 0x2 + (i * 0x20); j <= 0x9 + (i * 0x20); j++)
             {
                 name += (char)saveData[j];
@@ -55,6 +70,7 @@ DataField extends JTextField {
             charNames[i] = name;
         }
     }
+
 
     public DataField(int offset, int length) {
         this.offset = offset;
@@ -64,6 +80,7 @@ DataField extends JTextField {
         setText(Integer.toString(ByteToInt(data)));
         setColumns(length * 2);
 
+        // Commits changes in this textfield to the saveData when you press enter
         this.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -73,11 +90,13 @@ DataField extends JTextField {
         });
 
         this.addFocusListener(new FocusListener() {
+            // Automatically select everything in the textField when you click on it
             @Override
             public void focusGained(FocusEvent e) {
                 selectAll();
             }
 
+            // When you click away, commit all changes in the textfield to saveData
             @Override
             public void focusLost(FocusEvent e) {
                 setData(Integer.parseInt(getText()));
@@ -86,6 +105,7 @@ DataField extends JTextField {
 
     }
 
+    //Converts a byte array to an integer little endian
     private int ByteToInt(byte[] bytes){
         int newInt = 0;
         for(int b = 0; b < length; b++){
@@ -94,13 +114,17 @@ DataField extends JTextField {
         return newInt;
     }
 
+    //Converts an integer into bytes and stores them in saveData
     private void setData(int newData)
     {
         for(int b = 0; b < length; b++){
             data[b] = (byte)((newData >>> (b * 8)) & 0xFF);
+            //shift int by 8 bits for each iteration to get each byte and mask it to 8 bits to prevent bugs
+            //store the result in the data attribute of DataField
         }
         for(int b = offset; b < offset + length; b++){
             saveData[b] = data[b - offset];
+            // use stored data attribute and offset to modify the saveData we have locally
         }
     }
 
--- src/ultimaeditor/MainFrame.java
+++ src/ultimaeditor/MainFrame.java
@@ -1,11 +1,19 @@
+package ultimaeditor;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/** Class entirely for GUI, extends JFrame to make it happen
+ * The gui is made up of DataFields, each one represents an offset we would
+ * like to modify in Ultima V.
+ * For each one, we provide the offset and number of bytes we are modifying.
+ */
 public class MainFrame extends JFrame {
 
-    MainFrame() {
+    private MainFrame() {
+        // Determine basic layout and settings of GUI
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Ultima V Save Editor");
         setPreferredSize(new Dimension(500, 250));
@@ -16,15 +24,20 @@ public class MainFrame extends JFrame {
         JPanel row1 = new JPanel(new FlowLayout());
         JPanel row2 = new JPanel(new FlowLayout());
 
-
+        //Create a panel for each character that will hold its own values and offsets.
         JPanel cardPanel = new JPanel(new CardLayout());
         for(int i = 0; i < 16; i++)
         {
+            // Since each character is offset by 32 bits, we are able to reuse lots of the same code.
+            // makeCharacterField returns the entire panel we want to display.
             cardPanel.add(makeCharacterField(0x20 * i), DataField.charNames[i]);
         }
 
-        JComboBox chars = new JComboBox(DataField.charNames);
+        // Creates the dropdown menu using character names taken straight from the SAVED.GAM file
+        // The panels for each character created in the previous loop are tied to this
+        JComboBox chars = new JComboBox<>(DataField.charNames);
         chars.addActionListener(new ActionListener() {
+            // When we change what is selected in the dropdown, the current character stats being displayed are changed
             public void actionPerformed(ActionEvent actionEvent) {
                 CardLayout cl = (CardLayout)(cardPanel.getLayout());
                 cl.show(cardPanel, DataField.charNames[chars.getSelectedIndex()]);
@@ -32,7 +45,10 @@ public class MainFrame extends JFrame {
         });
 
 
-        row1.add(chars);
+        row1.add(chars); // add the character stats panel (initializes with main character being displayed)
+
+        // Each stat shared by the group is added here. We give a label to them, an offset, and the size of the data
+        // that we are storing. Datafield objects themselves are based on textfields and can be added directly to the GUI
         row1.add(makeDataField("Gold", 0x204, 2));
         row1.add(makeDataField("Keys", 0x206, 1));
         row1.add(makeDataField("Gems", 0x207, 1));
@@ -42,16 +58,17 @@ public class MainFrame extends JFrame {
         row2.add(makeDataField("M. Axe", 0x240, 1));
         topP.add(row1);
         topP.add(row2);
+
+        // Use the border layout to get things looking right
         this.add(topP, BorderLayout.NORTH);
-
         this.add(cardPanel, BorderLayout.CENTER);
-
-
         this.add(makeButtonPanel(), BorderLayout.SOUTH);
 
-        pack();
+        pack(); // size window based on contents
     }
 
+    // Using a given character offset (where the data begins describing a character) we are able to determine where all
+    // necessary attributes are stored and create DataField objects for each.
     private JPanel makeCharacterField(int offset){
         JPanel centerP = new JPanel(new GridLayout(0, 1));
         JPanel row1 = new JPanel(new FlowLayout());
@@ -70,6 +87,7 @@ public class MainFrame extends JFrame {
         return centerP;
     }
 
+    // Method to return a JPanel that stores a label with given text and a datafield (modified textfield) next to it
     private JPanel makeDataField(String label, int offset, int length){
         JPanel p = new JPanel(new FlowLayout());
         JLabel nameLabel = new JLabel(label + ": ", JLabel.LEADING);
@@ -79,6 +97,9 @@ public class MainFrame extends JFrame {
         return p;
     }
 
+    // Method that creates a panel containing two buttons:
+    // One that reloads all datafields and the main window itself
+    // Another that applies all the changes that have been made so far
     private JPanel makeButtonPanel(){
         JPanel p = new JPanel(new FlowLayout());
 
@@ -87,10 +108,10 @@ public class MainFrame extends JFrame {
         refreshButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                setVisible(false);
-                DataField.reload();
-                loadFrame();
-                dispose();
+                setVisible(false); // hide the current window
+                DataField.reload(); // reload save file
+                loadFrame(); // create a new window (with new values in datafields)
+                dispose(); // get rid of the current window (previously hidden)
             }
         });
 
@@ -99,7 +120,7 @@ public class MainFrame extends JFrame {
         applyButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                DataField.persist();
+                DataField.persist(); // call the static persist method that will save to disk
             }
         });
 
@@ -108,11 +129,13 @@ public class MainFrame extends JFrame {
         return p;
     }
 
-    public static void loadFrame(){
+    // Used to create a new main window
+    private static void loadFrame(){
         MainFrame f = new MainFrame();
         f.setVisible(true);
     }
 
+    //Entry point, simply opens the window
     public static void main(String args[]) {
         loadFrame();
     }
